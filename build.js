75faf538a4d6e10d2523430f7a94a994a62dd1bc
Kruithne <kruithne@gmail.com>
2019-10-22 14:38:31 +0100

    Implement foundation of build script

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsp = fs.promises;
+const AdmZip = require('adm-zip');
 const path = require('path');
+const util = require('util');
 const chalk = require('chalk');
+const request = require('request');
+const filesize = require('filesize');
 const argv = process.argv.splice(2);
 
 const CONFIG_FILE = path.resolve('./build.conf');
@@ -13,11 +18,13 @@ const log = {
 };
 
 (async () => {
-    const config = JSON.parse(await fs.readFile(CONFIG_FILE));
-
-    // Create the output directory if needed.
+    const config = JSON.parse(await fsp.readFile(CONFIG_FILE));
     const outDir = path.resolve(config.outputDirectory);
-    await fs.mkdir(outDir, { recursive: true });
+    const cacheDir = path.resolve(config.cacheDirectory);
+
+    // Create base directories we use during the build.
+    await fsp.mkdir(outDir, { recursive: true });
+    await fsp.mkdir(cacheDir, { recursive: true });
 
     // Index builds from the build config.
     const builds = new Map();
@@ -42,13 +49,77 @@ const log = {
     log.info('Selected builds: %s', targetBuilds.map(e => e.name).join(', '));
 
     for (const build of targetBuilds) {
-        log.info('Compiling build: %s', build.name);
+        log.info('Starting build \'%s\'...', build.name);
+        const buildStart = Date.now();
+        const buildDir = path.join(outDir, build.name);
 
-        // ToDo: Wipe the existing directory for the build (controlled by per-build flag).
+        // Wipe the build directory and then re-create it.
+        await fsp.rmdir(buildDir, { recursive: true });
+        await fsp.mkdir(buildDir, { recursive: true });
+
+        const bundleArchive = util.format(build.bundle, config.webkitVersion);
+        const bundlePath = path.join(cacheDir, bundleArchive);
+
+        // Download the bundle if it does not yet exist.
+        await fsp.access(bundlePath).catch(async () => {
+            const bundleURL = util.format(config.webkitURL, config.webkitVersion, bundleArchive);
+            log.info('Downloading %s...', bundleURL);
+
+            const startTime = Date.now();
+            await new Promise(resolve => request(bundleURL).pipe(fs.createWriteStream(bundlePath)).on('finish', resolve));
+
+            const elapsed = (Date.now() - startTime) / 1000;
+            const bundleStats = await fsp.stat(bundlePath);
+            log.success('Download complete! %s in %ds (%s/s)', filesize(bundleStats.size), elapsed, filesize(bundleStats.size / elapsed));
+        });
+
+        // Extract framework from the bundle.
+        const zip = new AdmZip(bundlePath);
+        const zipEntries = zip.getEntries();
+
+        // This function allows us to filter out files from the framework
+        // bundle that we don't want included in our final output.
+        const filter = config.webkitFilter;
+        const extractFilter = (entry) => {
+            // Whitelist takes priority over blacklist.
+            for (const check of filter.whitelist)
+                if (entry.match(check))
+                    return true;
+
+            for (const check of filter.blacklist)
+                if (entry.match(check))
+                    return false;
+
+            // Default to inclusion.
+            return true;
+        };
+
+        const extractStart = Date.now();
+        log.info('Extracting files from %s...', bundleArchive);
+
+        // ToDo: Unify this reduction to support other archive types.
+        const bundleName = path.basename(bundleArchive, '.zip');
+        for (const entry of zipEntries) {
+            const entryName = entry.entryName;
+            if (extractFilter(entryName)) {
+                const entryPath = entryName.substr(bundleName.length);
+                const entryDir = path.join(buildDir, path.dirname(entryPath));
+
+                await fsp.mkdir(entryDir, { recusrive: true });
+                zip.extractEntryTo(entryName, entryDir, false, true);
+            } else {
+                log.warn('Skipping extraction of \'%s\' due to filter!', entryName);
+            }
+        }
+
+        const extractElapsed = (Date.now() - extractStart) / 1000;
+        log.success('Extract %d files in %ds', zipEntries.length, extractElapsed);
+
         // ToDo: Copy over the source files (hard-copy or symlink, controlled by per-build flag).
         // ToDo: Minify/merge sources (controlled by per-build flag).
-        // ToDo: Download the required bundle (if not cached) and extract files.
-        // ToDo: Copy over any files from cache that are not blacklisted (locales, notification_helper.exe, etc).
+
+        const buildElapsed = (Date.now() - buildStart) / 1000;
+        log.success('Build \'%s\' completed in %ds', build.name, buildElapsed);
     }
 })().catch(e => {
     log.error('An unexpected error has halted the build:');
