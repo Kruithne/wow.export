c14235907fc0a44c2006476787004a8e8bfed549
Kruithne <kruithne@gmail.com>
2019-10-28 12:43:47 +0000

    Dynamically display the application version in the title bar

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-const test = 5;
+(async () => {
+    // Wait for the DOM to be loaded.
+    if (document.readyState === 'loading')
+        await new Promise(resolve => document.addEventListener('DOMContentLoaded', resolve));
+
+    // Append the application version to the title bar.
+    document.title += ' v' + nw.App.manifest.version;
+})();
