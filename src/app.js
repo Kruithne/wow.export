76f182f9241376a1ec3d5a94cac4edfe1d6ec85a
Kruithne <kruithne@gmail.com>
2019-10-28 12:44:47 +0000

    Prevent files being dropped onto the window

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+// Prevent files from being dropped onto the window.
+// ToDo: Expand this to allow local conversion invokes via file drop?
+window.ondragover = e => { e.preventDefault(); return false; };
+window.ondrop = e => { e.preventDefault(); return false; };
+
 (async () => {
     // Wait for the DOM to be loaded.
     if (document.readyState === 'loading')
