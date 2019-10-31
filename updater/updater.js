c3f78a31735b0871389a7f21da9c0397b7b9df2a
Kruithne <kruithne@gmail.com>
2019-10-31 11:44:17 +0000

    Copy over updated files in updater application

--- updater/updater.js
+++ updater/updater.js
@@ -1,4 +1,35 @@
 const argv = process.argv.splice(2);
+const fsp = require('fs').promises;
+const path = require('path');
+
+/**
+ * Returns an array of all files recursively collected from a directory.
+ * @param {string} dir Directory to recursively search.
+ * @param {array} out Array to be populated with results (automatically created).
+ */
+const collectFiles = async (dir, out = []) => {
+    const entries = await fsp.readdir(dir, { withFileTypes: true });
+    for await (const entry of entries) {
+        const entryPath = path.join(dir, entry.name);
+        if (entry.isDirectory())
+            await collectFiles(entryPath, out);
+        else
+            out.push(entryPath);
+    }
+
+    return out;
+};
+
+/**
+ * Wrapper for fs.Promises.access() to check if a directory exists.
+ * Returns a Promise that resolves to true or false.
+ * @param {string} dir 
+ */
+const directoryExists = async (dir) => {
+    return new Promise(resolve => {
+        fsp.access(dir).then(() => resolve(true)).catch(() => resolve(false));
+    });
+};
 
 (async () => {
     console.log('Applying updates, please wait!');
@@ -23,7 +54,23 @@ const argv = process.argv.splice(2);
         }
     }
 
-    // ToDo: Clone files from update directory.
+    const installDir = path.dirname(path.resolve(process.execPath));
+    const updateDir = path.join(installDir, '.update');
+
+    if (await directoryExists(updateDir)) {
+        const updateFiles = await collectFiles(updateDir);
+        for (const file of updateFiles) {
+            const relativePath = path.relative(updateDir, file);
+            const writePath = path.join(installDir, relativePath);
+
+            await fsp.copyFile(file, writePath).catch(() => {
+                console.log('UNABLE TO WRITE: %s', writePath);
+            });
+        }
+    } else {
+        console.log('Unable to locate update files.');
+    }
+
     // ToDo: Launch process once more.
     // ToDo: Terminate ourselves.
 })();
