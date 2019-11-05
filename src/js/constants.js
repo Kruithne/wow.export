f8ff316aa4c68dbe0c948338f17a521a28c3bb37
Kruithne <kruithne@gmail.com>
2019-11-05 14:29:44 +0000

    Improve documentation for constants

--- src/js/constants.js
+++ src/js/constants.js
@@ -6,29 +6,23 @@ const DATA_PATH = nw.App.dataPath;
 const UPDATER_EXT = { win32: '.exe', darwin: '.app' };
 
 module.exports = {
-    // General Constants
-    INSTALL_PATH,
-    DATA_PATH,
-    RUNTIME_LOG: path.join(DATA_PATH, 'runtime.log'),
+    INSTALL_PATH, // Path to the application installation.
+    DATA_PATH, // Path to the users data directory.
+    RUNTIME_LOG: path.join(DATA_PATH, 'runtime.log'), // Path to the runtime log.
+    MAX_RECENT_LOCAL: 3, // Maximum recent local installations to remember.
 
-    // How many recent local installations should we remember?
-    MAX_RECENT_LOCAL: 3,
-
-    // Configuration Constants
     CONFIG:  {
-        DEFAULT_PATH: path.join(INSTALL_PATH, 'src', 'default_config.jsonc'),
-        USER_PATH: path.join(DATA_PATH, 'config.json')
+        DEFAULT_PATH: path.join(INSTALL_PATH, 'src', 'default_config.jsonc'), // Path of default configuration file.
+        USER_PATH: path.join(DATA_PATH, 'config.json') // Path of user-defined configuration file.
     },
 
-    // Application Updates
     UPDATE: {
-        URL: 'https://kruithne.net/wow.export/%s/',
-        MANIFEST: 'package.json',
-        DIRECTORY: path.join(INSTALL_PATH, '.update'),
-        HELPER: 'updater' + (UPDATER_EXT[process.platform] || '')
+        URL: 'https://kruithne.net/wow.export/%s/', // Remote path to obtain updates from.
+        MANIFEST: 'package.json', // Remote manifest file for update checking.
+        DIRECTORY: path.join(INSTALL_PATH, '.update'), // Temporary directory for storing update data.
+        HELPER: 'updater' + (UPDATER_EXT[process.platform] || '') // Path to update helper application.
     },
 
-    // Product Keys
     // These are labelled as they appear in the Battle.net launcher.
     PRODUCTS: {
         'wow': 'World of Warcraft',
@@ -37,17 +31,15 @@ module.exports = {
         'wow_classic': 'World of Warcraft Classic'
     },
 
-    // Blizzard Patch Server
     PATCH: {
-        REGIONS: ['eu', 'us', 'kr', 'cn', 'tw'],
-        DEFAULT_REGION: 'us',
-        HOST: 'http://%s.patch.battle.net:1119/',
-        SERVER_CONFIG: '/cdns',
-        VERSION_CONFIG: '/versions'
+        REGIONS: ['eu', 'us', 'kr', 'cn', 'tw'], // Valid CDN regions.
+        DEFAULT_REGION: 'us', // Region which is selected by default.
+        HOST: 'http://%s.patch.battle.net:1119/', // Blizzard patch server host.
+        SERVER_CONFIG: '/cdns', // CDN config file on patch server.
+        VERSION_CONFIG: '/versions' // Versions config file on patch server.
     },
 
-    // Local Builds
     BUILD: {
-        MANIFEST: '.build.info'
+        MANIFEST: '.build.info' // File that contains version information in local installs.
     }
 };
