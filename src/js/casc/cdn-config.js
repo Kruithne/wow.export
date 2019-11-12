143b920fc0beae435de1996bc76755a47c9fb5fc
Kruithne <kruithne@gmail.com>
2019-11-12 22:47:07 +0000

    Normalize CDN config keys

--- src/js/casc/cdn-config.js
+++ src/js/casc/cdn-config.js
@@ -1,5 +1,23 @@
 const KEY_VAR_PATTERN = /([^\s]+)\s?=\s?(.*)/;
 
+/**
+ * Convert a config key such as 'encoding-sizes' to 'encodingSizes'.
+ * This helps keep things consistent when accessing key properties.
+ * @param {string} key 
+ */
+const normalizeKey = (key) => {
+	const keyParts = key.split('-');
+
+	// Nothing to split, just use the normal key.
+	if (keyParts.length === 1)
+		return key;
+
+	for (let i = 1; i < keyParts.length; i++)
+		keyParts[i][0] = keyParts[i][0].toUpperCase();
+
+	return keyParts.join();
+};
+
 module.exports = data => {
 	const entries = {};
 	let lines = data.split(/\r?\n/);
@@ -13,7 +31,7 @@ module.exports = data => {
 		if (match === null)
 			throw new Error('Invalid token encountered parsing CDN config');
 
-		entries[match[1]] = match[2];
+		entries[normalizeKey(match[1])] = match[2];
 	}
 	
 	return entries;
