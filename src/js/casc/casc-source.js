27ebb34c6d08aaef6ebe7beccbc8410e1e800be0
Kruithne <kruithne@gmail.com>
2019-11-12 21:30:26 +0000

    Replace LocaleFlags/ContentFlags with updated values

--- src/js/casc/casc-source.js
+++ src/js/casc/casc-source.js
@@ -5,40 +5,34 @@ const EMPTY_HASH = '00000000000000000000000000000000';
 const ENC_MAGIC = 0x4E45;
 
 const LocaleFlag = {
-	All: 0xFFFFFFFF,
-	None: 0,
-	enUS: 0x2,
-	koKR: 0x4,
-	frFR: 0x10,
-	deDE: 0x20,
-	zhCN: 0x40,
-	esES: 0x80,
-	zhTW: 0x100,
-	enGB: 0x200,
-	enCH: 0x400,
-	enTW: 0x800,
-	esMX: 0x1000,
-	ruRU: 0x2000,
-	ptBR: 0x4000,
-	itIT: 0x8000,
-	ptPT: 0x10000,
-	enSG: 0x20000000,
-	plPL: 0x40000000,
-	All_WoW: 0x1F3F6
+	enUS = 0x2,
+	koKR = 0x4,
+	frFR = 0x10,
+	deDE = 0x20,
+	zhCN = 0x40,
+	esES = 0x80,
+	zhTW = 0x100,
+	enGB = 0x200,
+	enCN = 0x400,
+	enTW = 0x800,
+	esMX = 0x1000,
+	ruRU = 0x2000,
+	ptBR = 0x4000,
+	itIT = 0x8000,
+	ptPT = 0x10000
 };
 
 const ContentFlag = {
-	None: 0,
-	F00000001: 0x1,
-	F00000002: 0x2,
-	F00000004: 0x4,
-	F00000008: 0x8,
-	F00000010: 0x10,
-	LowViolence: 0x80,
-	F10000000: 0x10000000,
-	F20000000: 0x20000000,
-	Bundle: 0x40000000,
-	NoCompression: 0x80000000
+	LoadOnWindows = 0x8,
+	LoadOnMacOS = 0x10,
+	LowViolence = 0x80,
+	DoNotLoad = 0x100,
+	UpdatePlugin = 0x800,
+	Encrypted = 0x8000000,
+	NoNameHash = 0x10000000,
+	UncommonResolution = 0x20000000,
+	Bundle = 0x40000000,
+	NoCompression = 0x80000000
 };
 
 class CASC {
