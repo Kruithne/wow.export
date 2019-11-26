0cfcdde36d8e7105e0c3e90cfcacf4d8a252406c
Kruithne <kruithne@gmail.com>
2019-11-26 16:34:38 +0000

    Provide documentation for listbox component

--- src/js/components/listbox.js
+++ src/js/components/listbox.js
@@ -1,5 +1,13 @@
 Vue.component('listbox', {
+	/**
+	 * component.items should contain a reference to an array
+	 * containing items to display in the listbox.
+	 */
 	props: ['items'],
+
+	/**
+	 * Reactive instance data.
+	 */
 	data: function() {
 		return {
 			scroll: 0,
@@ -9,6 +17,11 @@ Vue.component('listbox', {
 		}
 	},
 
+	/**
+	 * Invoked when the component is mounted.
+	 * Used to register global mouse listeners and size observer.
+	 * ToDo: Unregister these events if the components ever get destroyed.
+	 */
 	mounted: function() {
 		document.addEventListener('mousemove', e => this.moveMouse(e));
 		document.addEventListener('mouseup', e => this.stopMouse(e));
@@ -17,24 +30,44 @@ Vue.component('listbox', {
 	},
 
 	computed: {
+		/**
+		 * Offset of the scroll widget in pixels.
+		 * Between 0 and the height of the component.
+		 */
 		scrollOffset: function() {
 			return (this.scroll) + 'px';
 		},
 
+		/**
+		 * Index which array reading should start at, based on the current
+		 * relative scroll and the overal item count. Value is dynamically
+		 * capped based on slot count to prevent empty slots appearing.
+		 */
 		scrollIndex: function() {
 			return Math.floor((this.$props.items.length - this.slotCount) * this.scrollRel);
 		},
 
+		/**
+		 * Dynamic array of items which should be displayed from the underlying
+		 * data array. Reactively updates based on scroll and data.
+		 */
 		filteredItems: function() {
 			return this.$props.items.slice(this.scrollIndex, this.scrollIndex + this.slotCount);
 		},
 
+		/**
+		 * Weight (0-1) of a single item.
+		 */
 		itemWeight: function() {
 			return 1 / this.$props.items.length;
 		}
 	},
 
 	methods: {
+		/**
+		 * Invoked by a ResizeObserver when the main component node
+		 * is resized due to layout changes.
+		 */
 		resize: function() {
 			const max = this.$el.clientHeight - (this.$refs.scroller.clientHeight);
 			this.scroll = max * this.scrollRel;
@@ -56,18 +89,30 @@ Vue.component('listbox', {
 			this.slotCount = Math.ceil(this.$el.clientHeight / this.childHeight);
 		},
 
+		/**
+		 * Restricts the scroll offset to prevent overflowing and
+		 * calculates the relative (0-1) offset based on the scroll.
+		 */
 		recalculateBounds: function() {
 			const max = this.$el.clientHeight - (this.$refs.scroller.clientHeight);
 			this.scroll = Math.min(max, Math.max(0, this.scroll));
 			this.scrollRel = this.scroll / max;
 		},
 
+		/**
+		 * Invoked when a mouse-down event is captured on the scroll widget.
+		 * @param {MouseEvent} e 
+		 */
 		startMouse: function(e) {
 			this.scrollStartY = e.clientY;
 			this.scrollStart = this.scroll;
 			this.isScrolling = true;
 		},
 
+		/**
+		 * Invoked when a mouse-move event is captured globally.
+		 * @param {MouseEvent} e 
+		 */
 		moveMouse: function(e) {
 			if (this.isScrolling) {
 				this.scroll = this.scrollStart + (e.clientY - this.scrollStartY);
@@ -75,10 +120,18 @@ Vue.component('listbox', {
 			}
 		},
 
+		/**
+		 * Invoked when a mouse-up event is captured globally.
+		 * @param {MouseEvent} e 
+		 */
 		stopMouse: function(e) {
 			this.isScrolling = false;
 		},
 
+		/**
+		 * Invoked when a mouse-wheel event is captured on the component node.
+		 * @param {WheelEvent} e
+		 */
 		wheelMouse: function(e) {
 			const weight = this.$el.clientHeight - (this.$refs.scroller.clientHeight);
 			this.scroll += ((e.deltaY / 10) * this.itemWeight) * weight;
@@ -86,6 +139,9 @@ Vue.component('listbox', {
 		}
 	},
 
+	/**
+	 * HTML mark-up to render for this component.
+	 */
 	template: `<div class="ui-listbox" @wheel="wheelMouse">
 		<div class="scroller" ref="scroller" @mousedown="startMouse" :class="{ using: isScrolling }" :style="{ top: scrollOffset }"><div></div></div>
 		<div v-for="item in filteredItems" class="item">{{ item }}</div>
