9bb89bc1b0406694ff13af50234b77e2d0d3cc58
Kruithne <kruithne@gmail.com>
2019-11-28 18:07:52 +0000

    Add documentation to the MenuButton component

--- src/js/components/menubutton.js
+++ src/js/components/menubutton.js
@@ -1,28 +1,46 @@
 const util = require('util');
 
 Vue.component('menu-button', {
+	/**
+	 * options: An array of strings denoting options shown in the menu.
+	 * label: Formattable button label. %s is substituted for the selected option.
+	 * default: Which option to use as a default.
+	 * disabled: Controls disabled state of the component.
+	 */
 	props: ['options', 'label', 'default', 'disabled'],
 
 	data: function() {
 		return {
-			selectedOption: '',
-			open: false
+			selectedOption: '', // Currently selected option.
+			open: false // If the menu is open or not.
 		}
 	},
 
 	methods: {
+		/**
+		 * Set the selected option for this menu button.
+		 * @param {string} option 
+		 */
 		select: function(option) {
 			this.open = false;
 			this.selectedOption = option;
 			this.$emit('change', option);
 		},
 
+		/**
+		 * Attempt to open the menu.
+		 * Respects component disabled state.
+		 */
 		openMenu: function() {
 			this.open = !this.open && !this.disabled;
 		}
 	},
 
 	computed: {
+		/**
+		 * The currently selected option.
+		 * Will return default if the selected option is not a valid option.
+		 */
 		selected: function() {
 			if (this.options.includes(this.selectedOption))
 				return this.selectedOption;
@@ -30,11 +48,17 @@ Vue.component('menu-button', {
 			return this.default;
 		},
 
+		/**
+		 * Returns the formatted text to display on the button.
+		 */
 		displayText: function() {
 			return util.format(this.label, this.selected);
 		}
 	},
 
+	/**
+	 * HTML mark-up to render for this component.
+	 */
 	template: `<div class="ui-menu-button" :class="{ disabled }">
 		<input type="button" :value="displayText" :class="{ disabled }" @click="$emit('click', $event)"/>
 		<div class="arrow" @click="openMenu"></div>
