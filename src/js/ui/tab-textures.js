01d653a3ed9050f0d28915e73f58b198226d38b2
Kruithne <kruithne@gmail.com>
2019-11-28 16:28:27 +0000

    Add prototype structure for texture viewer exporting functionality

--- src/js/ui/tab-textures.js
+++ src/js/ui/tab-textures.js
@@ -5,6 +5,7 @@ const BLPFile = require('../casc/blp');
 
 let isLoading = false;
 let selectedFile = null;
+let userSelection = [];
 
 let previewContainer = null;
 let previewInner = null;
@@ -53,8 +54,48 @@ core.events.once('init', () => {
 
 	// Track selection changes on the texture listbox and preview first texture.
 	core.events.on('user-select-texture', async selection => {
+		// Store the full selection for exporting purposes.
+		userSelection = selection;
+
+		// Check if the first file in the selection is "new".
 		const first = selection[0];
 		if (!isLoading && first && selectedFile !== first)
 			previewTexture(first);
+	});
+
+	// Track when the user clicks to export selected textures.
+	core.events.on('click-export-texture', async () => {
+		const count = userSelection.length;
+		if (count === 0) {
+			core.setToast('info', 'You didn\'t select any files to export; you should do that first.', {}, 10000);
+			return;
+		}
+
+		core.view.isBusy++;
+		let toastTag = count > 1 ? count + ' textures' : userSelection[0];
+		core.setToast('progress', 'Exporting ' + toastTag + ', please wait...');
+
+		let failed = 0;
+		for (const fileName of userSelection) {
+			try {
+				const file = await core.view.casc.getFileByName(fileName);
+				// ToDo: Actually export somewhere.
+			} catch (e) {
+				failed++;
+				log.write('Failed to export %s (%s)', fileName, e.message);
+			}
+		}
+
+		if (failed === 0) {
+			// Everything exported successfully.
+			core.setToast('success', 'Successfully exported ' + toastTag, {}, 10000);
+		} else if (failed < count) {
+			// Partial success, some files failed.
+			core.setToast('info', util.format('Export complete, but %d files failed to export', failed), { 'View Log': () => log.openRuntimeLog() }, 10000);
+		} else {
+			// Everything failed to export, prompt user with log link.
+			core.setToast('error', 'Unable to export ' + toastTag, { 'View Log': () => log.openRuntimeLog() }, 10000);
+		}
+		core.view.isBusy--;
 	});
 });
