ee18b181937b8b1b776767f837235152938dbadf
Kruithne <kruithne@gmail.com>
2019-12-06 12:04:03 +0000

    Add logging call to preview screenshot export

--- src/js/ui/tab-models.js
+++ src/js/ui/tab-models.js
@@ -116,6 +116,7 @@ const exportFiles = async (files, isLocal = false) => {
 			const buf = await BufferWrapper.fromCanvas(canvas, 'image/png');
 			await buf.writeToFile(ExportHelper.replaceExtension(exportPath, '.png'));
 
+			log.write('Saved 3D preview screenshot to %s', exportPath);
 			core.setToast('success', util.format('Successfully exported preview to %s!', exportPath));
 		} else {
 			core.setToast('error', 'The PNG export option only works for model previews. Preview something first!');
