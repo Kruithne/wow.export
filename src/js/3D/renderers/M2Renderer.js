a33cca2a850e157962b0bb83bd36472843943e4d
Kruithne <kruithne@gmail.com>
2019-12-08 15:18:08 +0000

    Dereference geosetWatcher after incoking it

--- src/js/3D/renderers/M2Renderer.js
+++ src/js/3D/renderers/M2Renderer.js
@@ -164,8 +164,10 @@ class M2Renderer {
 	 */
 	dispose() {
 		// Unregister geoset array watcher.
-		if (this.geosetWatcher)
+		if (this.geosetWatcher) {
 			this.geosetWatcher();
+			this.geosetWatcher = undefined;
+		}
 
 		// Release bound textures.
 		for (const tex of this.textures)
