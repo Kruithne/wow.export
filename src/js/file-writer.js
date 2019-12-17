80615264f0b16826d63d5b8ddf4806fb488348d1
Kruithne <kruithne@gmail.com>
2019-12-17 15:47:01 +0000

    Refactor FileWriter from streamed to single-write
    Resolves GH-71

--- src/js/file-writer.js
+++ src/js/file-writer.js
@@ -7,64 +7,24 @@ class FileWriter {
 	 * @param {string} encoding 
 	 */
 	constructor(file, encoding = 'utf8') {
-		this.isBlocked = false;
-		this.isClosing = false;
+		this.file = file;
+		this.encoding = encoding;
 		this.queue = [];
-		this.stream = fs.createWriteStream(file, encoding);
-		this.stream.on('drain', () => this.onDrain());
-	}
-
-	/**
-	 * Invoked when the stream is drained.
-	 */
-	onDrain() {
-		// Drain the queue as much as we can.
-		while (this.queue.length > 0)
-			if (!this.stream.write(this.queue.shift()))
-				return;
-
-		if (this.isClosing) {
-			// Writer has been closed, finish up here.
-			this.stream.end();
-		} else {
-			// Write still open, unblock queue.
-			this.isBlocked = false;
-		}
 	}
 
 	/**
 	 * Write a line to the file.
 	 * @param {string} line 
 	 */
-	writeLine(line) {
-		line = line + '\n';
-		
-		if (this.isBlocked) {
-			// The stream is currently flushing, queue lines.
-			this.queue.push(line);
-		} else {
-			// No backlog, write directly to stream.
-			this.isBlocked = !this.stream.write(line);
-		}
+	writeLine(line) {	
+		this.queue.push(line);
 	}
 
 	/**
 	 * Close the stream.
 	 */
 	async close() {
-		return new Promise(res => {
-			// Resolve this promise once the stream is done.
-			this.stream.once('finish', res);
-
-			// Mark our writer as closing so that if we're draining, the
-			// queue can be fed to the stream before we actually close.
-			this.isClosing = true;
-
-			// If we're blocked, we need to wait for the queue to empty into
-			// the system to be flushed before trying to end.
-			if (!this.isBlocked)
-				this.stream.end();
-		});
+		await fsp.writeFile(this.file, this.queue.join('\n'), this.encoding);
 	}
 }
 
