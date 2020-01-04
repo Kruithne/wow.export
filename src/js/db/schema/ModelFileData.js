cac33eb338aa79520a3d2d857014b8fd707f7827
Martin Benjamins <marlamin@marlamin.com>
2020-01-04 20:46:25 +0100

    Add schema for ModelFileData

---
+++ src/js/db/schema/ModelFileData.js
@@ -0,0 +1,8 @@
+const FieldType = require('../FieldType');
+
+module.exports = {
+	FileDataID: FieldType.Int32,
+	Flags: FieldType.UInt8,
+	LodCount: FieldType.UInt8,
+	ModelResourcesID: FieldType.UInt32
+};
