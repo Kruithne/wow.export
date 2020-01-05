3d50e249fab4d9aef167322de47938d40a159015
Martin Benjamins <marlamin@marlamin.com>
2020-01-06 00:39:02 +0100

    Schema fixes

--- src/js/db/schema/GroundEffectTexture.js
+++ src/js/db/schema/GroundEffectTexture.js
@@ -1,9 +1,8 @@
 const FieldType = require('../FieldType');
 
 module.exports = {
-	EntryID: FieldType.UInt32,
-	Sound: FieldType.UInt8,
 	Density: FieldType.UInt32,
+	Sound: FieldType.UInt8,
 	DoodadID: [FieldType.UInt16, 4]
 	// DoodadWeights
 };
