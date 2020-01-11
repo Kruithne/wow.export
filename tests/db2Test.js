7ed5fb778bc55ba367f4fadbbf2fa5036209fc94
Martin Benjamins <marlamin@marlamin.com>
2020-01-11 15:13:47 +0100

    Test both mainline & classic WoW CreatureDisplayInfo (different compression)

--- tests/db2Test.js
+++ tests/db2Test.js
@@ -13,7 +13,7 @@ const FieldType = require ('../src/js/db/FieldType');
 })();
 
 (async () => {
-	// CreatureDisplayInfo
+	// CreatureDisplayInfo (Mainline WoW)
 	const CDIData = await BufferWrapper.readFile('./tests/resources/db2/CreatureDisplayInfo.db2');
 	const CDIDB = new WDCReader('DBFilesClient/CreatureDisplayInfo.db2', DB_CreatureDisplayInfo);
 	CDIDB.parse(CDIData);
@@ -23,8 +23,10 @@ const FieldType = require ('../src/js/db/FieldType');
 	{
 		ID: 93456,
 		ModelID: 10662,
+		SoundID: 0,
 		SizeClass: 1,
 		CreatureModelScale: 1.3,
+		CreatureModelAlpha: 255,
 		BloodID: 0,
 		ExtendedDisplayInfoID: 0,
 		NPCSoundID: 0,
@@ -48,6 +50,48 @@ const FieldType = require ('../src/js/db/FieldType');
 
 	const ourRecord = CDIDB.getRow(93456);
 	for (const [name, value] of Object.entries(properRecord)) {
+		assert.deepStrictEqual(value, ourRecord[name], 'Mismatch for column ' + name + ', proper value: ' + value + ' (' + value.toString(2) + '), our value: ' + ourRecord[name] + ' (' + ourRecord[name].toString(2) +')');
+	}
+})();
+
+(async () => {
+	// CreatureDisplayInfo (Classic WoW)
+	const CDIData = await BufferWrapper.readFile('./tests/resources/db2/CreatureDisplayInfo classic.db2');
+	const CDIDB = new WDCReader('DBFilesClient/CreatureDisplayInfo.db2', DB_CreatureDisplayInfo);
+	CDIDB.parse(CDIData);
+
+	// ID 38 as of 1.13.3.32887 (retrieved from WoW.tools), if DB2 in test is updated to newer version please double check if info is still correct.
+	const properRecordClassic =
+	{
+		ID: 38,
+		ModelID: 38,
+		SoundID: 0,
+		SizeClass: -1,
+		CreatureModelScale: 1.15,
+		CreatureModelAlpha: 255,
+		BloodID: 0,
+		ExtendedDisplayInfoID: 0,
+		NPCSoundID: 0,
+		ParticleColorID: 0,
+		PortraitCreatureDisplayInfoID: 0,
+		PortraitTextureFileDataID: 0,
+		ObjectEffectPackageID: 0,
+		AnimReplacementSetID: 0,
+		Flags: 0,
+		StateSpellVisualKitID: 0,
+		PlayerOverrideScale: 0,
+		PetInstanceScale: 1,
+		UnarmedWeaponType: -1,
+		MountPoofSpellVisualKitID: 0,
+		DissolveEffectID: 0,
+		Gender: 2,
+		DissolveOutEffectID: 0,
+		CreatureModelMinLod: 0,
+		TextureVariationFileDataID: [126123, 0, 0],
+	};
+
+	const ourRecord = CDIDB.getRow(38);
+	for (const [name, value] of Object.entries(properRecordClassic)) {
 		assert.deepStrictEqual(value, ourRecord[name], 'Mismatch for column ' + name + ', proper value: ' + value + ' (' + value.toString(2) + '), our value: ' + ourRecord[name] + ' (' + ourRecord[name].toString(2) +')');
 	}
 })();
