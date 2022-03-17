d8019908448931456b77c744583adbdd7a349db9
Kruithne <kruithne@gmail.com>
2022-03-17 10:54:12 +0000

    Fix state issues with context menu components

--- src/js/components/context-menu.js
+++ src/js/components/context-menu.js
@@ -39,7 +39,7 @@ Vue.component('context-menu', {
 		this.isLeft = this.positionX > window.innerWidth / 2;
 	},
 
-	template: `<div class="context-menu" v-if="!node" :class=" { low: isLow, left: isLeft }" :style="{ top: positionY + 'px', left: positionX + 'px' }" @mouseleave="$emit('close')" @click="$emit('close')">
+	template: `<div class="context-menu" v-if="node !== null && node !== false" :class=" { low: isLow, left: isLeft }" :style="{ top: positionY + 'px', left: positionX + 'px' }" @mouseleave="$emit('close')" @click="$emit('close')">
 		<div class="context-menu-zone"></div>
 		<slot v-bind:node="node"></slot>
 	</div>
