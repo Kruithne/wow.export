ec2933ef65094c32003917f6fe0876aea2bce5a5
Kruithne <kruithne@gmail.com>
2023-02-03 11:30:34 +0000

    remove unused import in file-field

--- src/app/components/file-field.ts
+++ src/app/components/file-field.ts
@@ -1,7 +1,5 @@
 /* Copyright (c) wow.export contributors. All rights reserved. */
 /* Licensed under the MIT license. See LICENSE in project root for license information. */
-import { ComponentData } from './component-base';
-
 export default {
 	props: ['value'],
 
