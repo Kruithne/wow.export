87bbf1eedd9e077eeb26f8f4a6e675a7b5a40415
Kruithne <kruithne@gmail.com>
2023-02-17 15:41:16 +0000

    debug: print emitted events to log for debugging

--- src/app/events.ts
+++ src/app/events.ts
@@ -1,6 +1,7 @@
 /* Copyright (c) wow.export contributors. All rights reserved. */
 /* Licensed under the MIT license. See LICENSE in project root for license information. */
 import EventEmitter from 'node:events';
+import Log from './log';
 
 export default new class extends EventEmitter {
 	constructor() {
@@ -9,11 +10,23 @@ export default new class extends EventEmitter {
 	}
 
 	/**
+	 * Emits an event and logs it.
+	 * @param event - The event to emit
+	 * @param args - The arguments to pass to the event listeners
+	 * @returns Whether the event was emitted
+	 */
+	emit(event: string, ...args: any[]): boolean {
+		Log.write('Event fired: ' + event);
+		return super.emit(event, ...args);
+	}
+
+	/**
 	 * Emits an event and awaits all listeners to complete.
 	 * @param event - The event to emit
 	 * @param args - The arguments to pass to the event listeners
 	 */
 	async emitAndAwait(event: string, ...args: any[]): Promise<void> {
+		Log.write('Event fired: ' + event);
 		const listeners = this.listeners(event).map((listener) => listener(...args));
 		await Promise.all(listeners);
 	}
