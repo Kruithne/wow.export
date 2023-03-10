c1745b793461e0743e87a3c259b1dcd6b5464092
Martin Benjamins <marlamin@marlamin.com>
2023-03-10 17:07:27 +0100

    Read new WDC4 section to fix crash

--- src/app/db/WDCReader.ts
+++ src/app/db/WDCReader.ts
@@ -337,6 +337,13 @@ export default class WDCReader {
 		// Ensure we've read the expected amount of pallet data.
 		assert.strictEqual(data.offset, prevPos + palletDataSize, 'Read incorrect amount of pallet data');
 
+		if (wdcVersion === 4) {
+			for (let sectionIndex = 0; sectionIndex < sectionCount - 1; sectionIndex++) {
+				const entryCount = data.readUInt32();
+				data.move(entryCount * 4);
+			}
+		}
+
 		prevPos = data.offset;
 
 		// char common_data[header.common_data_size];
