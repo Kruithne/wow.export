e2fd3d387498b05df287fdba257a2decb7c4a738
Kruithne <kruithne@gmail.com>
2023-05-30 20:08:28 +0100

    initial server foundation

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+/* Copyright (c) wow.export contributors. All rights reserved. */
+/* Licensed under the MIT license. See LICENSE in project root for license information. */
+
+const server = Bun.serve({
+	port: 3001, // Do not change without consulting @Kruithne
+
+	fetch(req) {
+		console.log(req);
+		return new Response('You\'ve reached wow.export.net, please leave a message after the beep.');
+	},
+});
+
+console.log(`Ready for connections ${server.port}...`);
