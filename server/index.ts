d71e054cb94f16a844080a28b30123d8ebe873f4
Kruithne <kruithne@gmail.com>
2023-05-30 22:16:34 +0100

    implement basic error handling

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,10 @@
 /* Copyright (c) wow.export contributors. All rights reserved. */
 /* Licensed under the MIT license. See LICENSE in project root for license information. */
+import http from 'http';
+
+function make_generic_response(status_code: number): Response {
+	return new Response(http.STATUS_CODES[status_code], { status: status_code });
+}
 
 const server = Bun.serve({
 	port: 3001, // Do not change without consulting @Kruithne
@@ -9,6 +14,11 @@ const server = Bun.serve({
 		console.log(req);
 		return new Response('You\'ve reached wow.export.net, please leave a message after the beep.');
 	},
+
+	error(error: Error) {
+		console.error(error);
+		return make_generic_response(500);
+	}
 });
 
 console.log(`Ready for connections ${server.port}...`);
