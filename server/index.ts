4124e5246959a98a5e37c278bcf5d9b8264a80aa
Kruithne <kruithne@gmail.com>
2023-05-31 22:26:36 +0100

    implement /services/internal/head

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ const server = Bun.serve({
 	port: 3001, // Do not change without consulting @Kruithne
 	development: false,
 
-	fetch(req) {
+	async fetch(req) {
 		const url = new URL(req.url);
 
 		// /services/internal/update is called from the automatic deployment workflow on GitHub
@@ -23,6 +23,18 @@ const server = Bun.serve({
 
 			server.stop(true); // TODO: Implement convinient-termination for this.
 			return make_generic_response(200); // OK
+		}
+
+		// /services/internal/head is called from the automatic deployment workflow on GitHub
+		// to check the current commit hash of the server.
+		if (url.pathname === '/services/internal/head') {
+			if (url.searchParams.get('key') !== process.env.WOW_EXPORT_SERVER_DEPLOY_KEY)
+				return make_generic_response(401); // Unauthorized
+
+			const git = Bun.spawn(['git', 'rev-parse', 'HEAD']);
+
+			// TODO: Fortify this against potentially returning an error.
+			return new Response(git.stdout, { status: 200 });
 		}
 
 		return make_generic_response(404); // Not found
