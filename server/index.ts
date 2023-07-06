a848bc555de3ba5fff0894cf9731aa2914bd101c
Kruithne <kruithne@gmail.com>
2023-07-06 17:12:14 +0100

    harden /services/internal/head against errors

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) wow.export contributors. All rights reserved. */
 /* Licensed under the MIT license. See LICENSE in project root for license information. */
-import { serve, ServerStop } from 'spooder';
+import { serve, caution, ServerStop } from 'spooder';
 
 const server = serve(3001);
 
@@ -12,11 +12,40 @@ server.route('/services/internal/update', (req: Request, url: URL) => {
 	return 200;
 });
 
-server.route('/services/internal/head', (req: Request, url: URL) => {
+server.route('/services/internal/head', async (req: Request, url: URL) => {
 	if (url.searchParams.get('key') !== process.env.WOW_EXPORT_SERVER_DEPLOY_KEY)
 		return 401;
 
-	// TODO: Fortify this against potentially returning an error.
 	const git = Bun.spawn(['git', 'rev-parse', 'HEAD']);
-	return git.stdout;
+	const chunks = [];
+
+	if (!git.stdout) {
+		caution('failed to spawn git process', {
+			cmd: 'git rev-parse HEAD',
+			endpoint: '/services/internal/head'
+		});
+
+		return 500;
+	}
+
+	// TODO: Replace this with TextDecoderStream once it's available in bun.
+	// See https://github.com/oven-sh/bun/issues/159
+	for await (const chunk of git.stdout)
+		chunks.push(chunk);
+
+	const output = new TextDecoder().decode(Uint8Array.from(chunks));
+
+	// Expecting 40 hex characters followed by a newline.
+	if (!/^[a-f0-9]{40}\n$/.test(output)) {
+		caution('git rev-parse HEAD returned unexpected output', {
+			cmd: 'git rev-parse HEAD',
+			endpoint: '/services/internal/head',
+			chunk_count: chunks.length,
+			output
+		});
+
+		return 500;
+	}
+
+	return output.trim();
 });
