2227f48183521cbc5a4a3640615d4a83dcae04b2
Martin Benjamins <marlamin@marlamin.com>
2023-09-25 22:55:53 +0200

    Disable data tab initialization
    api.wow.tools should not be relied on in any case

--- src/js/ui/tab-data.js
+++ src/js/ui/tab-data.js
@@ -4,53 +4,55 @@
 	License: MIT
  */
 const core = require('../core');
-const log = require('../log');
-const generics = require('../generics');
-const listfile = require('../casc/listfile');
-const WDCReader = require('../db/WDCReader');
-const path = require('path');
+// const log = require('../log');
+// const generics = require('../generics');
+// const listfile = require('../casc/listfile');
+// const WDCReader = require('../db/WDCReader');
+// const path = require('path');
 
-let selectedFile = null;
-let db2NameMap = undefined;
+// let selectedFile = null;
+// let db2NameMap = undefined;
 
 core.registerLoadFunc(async () => {
-	log.write('Downloading DB2 filename mapping from %s', "https://api.wow.tools/databases/");
-	generics.getJSON("https://api.wow.tools/databases/").then(raw => db2NameMap = raw);
+	return;
 
-	// Track selection changes on the text listbox and set first as active entry.
-	core.view.$watch('selectionDB2s', async selection => {
-		// Check if the first file in the selection is "new".
-		const first = listfile.stripFileEntry(selection[0]);
-		if (!core.view.isBusy && first && selectedFile !== first && db2NameMap !== undefined) {
-			try {
-				const lowercaseTableName = path.basename(first, '.db2');
-				const tableName = db2NameMap.find(e => e.name == lowercaseTableName)?.displayName;
+	// log.write('Downloading DB2 filename mapping from %s', "https://api.wow.tools/databases/");
+	// generics.getJSON("https://api.wow.tools/databases/").then(raw => db2NameMap = raw);
 
-				const db2Reader = new WDCReader('DBFilesClient/' + tableName + '.db2');
-				await db2Reader.parse();
+	// // Track selection changes on the text listbox and set first as active entry.
+	// core.view.$watch('selectionDB2s', async selection => {
+	// 	// Check if the first file in the selection is "new".
+	// 	const first = listfile.stripFileEntry(selection[0]);
+	// 	if (!core.view.isBusy && first && selectedFile !== first && db2NameMap !== undefined) {
+	// 		try {
+	// 			const lowercaseTableName = path.basename(first, '.db2');
+	// 			const tableName = db2NameMap.find(e => e.name == lowercaseTableName)?.displayName;
+
+	// 			const db2Reader = new WDCReader('DBFilesClient/' + tableName + '.db2');
+	// 			await db2Reader.parse();
 				
-				core.view.tableBrowserHeaders = [...db2Reader.schema.keys()];
+	// 			core.view.tableBrowserHeaders = [...db2Reader.schema.keys()];
 
-				const rows = db2Reader.getAllRows();
-				if (rows.size == 0) 
-					core.setToast('info', 'Selected DB2 has no rows.', null);
-				else 
-					core.hideToast(false);
+	// 			const rows = db2Reader.getAllRows();
+	// 			if (rows.size == 0) 
+	// 				core.setToast('info', 'Selected DB2 has no rows.', null);
+	// 			else 
+	// 				core.hideToast(false);
 
-				const parsed = Array(rows.size);
+	// 			const parsed = Array(rows.size);
 
-				let index = 0;
-				for (const row of rows.values())
-					parsed[index++] = Object.values(row);
+	// 			let index = 0;
+	// 			for (const row of rows.values())
+	// 				parsed[index++] = Object.values(row);
 
-				core.view.tableBrowserRows = parsed;
+	// 			core.view.tableBrowserRows = parsed;
 
-				selectedFile = first;
-			} catch (e) {
-				// Error reading/parsing DB2 file.
-				core.setToast('error', 'Unable to open DB2 file ' + first, { 'View Log': () => log.openRuntimeLog() }, -1);
-				log.write('Failed to open CASC file: %s', e.message);
-			}
-		}
-	});
+	// 			selectedFile = first;
+	// 		} catch (e) {
+	// 			// Error reading/parsing DB2 file.
+	// 			core.setToast('error', 'Unable to open DB2 file ' + first, { 'View Log': () => log.openRuntimeLog() }, -1);
+	// 			log.write('Failed to open CASC file: %s', e.message);
+	// 		}
+	// 	}
+	// });
 });
