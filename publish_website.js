f60105dbdf70aad13cccaa9bcf70c3947e76762a
Kruithne <kruithne@gmail.com>
2023-09-26 16:35:25 +0100

    add website deployment script/workflow

---
+++ publish_website.js
@@ -0,0 +1,44 @@
+/*!
+	wow.export (https://github.com/Kruithne/wow.export)
+	Authors: Kruithne <kruithne@gmail.com>
+	License: MIT
+ */
+const fs = require('fs');
+const fsp = fs.promises;
+const SFTPClient = require('ssh2-sftp-client');
+
+(async () => {
+	let sftp;
+
+	try {
+		const sftp_config = {
+			host: process.env.SFTP_HOST,
+			port: process.env.SFTP_PORT ?? 22,
+			username: process.env.SFTP_USER,
+			password: process.env.SFTP_PASS,
+			privateKey: process.env.SFTP_PRIVATE_KEY,
+			remoteUpdateDir: process.env.SFTP_REMOTE_UPDATE_DIR,
+			remotePackageDir: process.env.SFTP_REMOTE_PACKAGE_DIR
+		};
+
+		// Load private key from disk if defined.
+		if (typeof sftp_config.privateKey === 'string') {
+			console.log('Loading private key...');
+			sftp_config.privateKey = await fsp.readFile(sftp_config.privateKey);
+		}
+
+		console.log('Establishing SFTP connection...');
+
+		sftp = new SFTPClient();
+		await sftp.connect();
+
+		await sftp.mkdir(sftp_config.remoteUpdateDir, true);
+		await sftp.uploadDir('./website', sftp_config.remoteUpdateDir);
+	} catch (e) {
+		console.error('Publish failed due to error: %s', e.message);
+		console.error(e.stack);
+	} finally {
+		if (sftp)
+			await sftp.end();
+	}
+})();
