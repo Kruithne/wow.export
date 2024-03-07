d03e85342b3ed940b14d16abe98d22247b51216a
David Rios <david.rios.gomes@gmail.com>
2024-03-07 15:40:32 -0300

    Make emissive option on by default

--- addons/blender/2.80/io_scene_wowobj/__init__.py
+++ addons/blender/2.80/io_scene_wowobj/__init__.py
@@ -52,9 +52,9 @@ class Settings:
     importGOBJ = True
     importTextures = True
     useTerrainBlending = True
-    createEmissiveMaterials = False
+    createEmissiveMaterials = True
 
-    def __init__(self, useAlpha = True, createVertexGroups = False, allowDuplicates = False, importWMO = True, importWMOSets = True, importM2 = True, importGOBJ = True, importTextures = True, useTerrainBlending = True, createEmissiveMaterials = False):
+    def __init__(self, useAlpha = True, createVertexGroups = False, allowDuplicates = False, importWMO = True, importWMOSets = True, importM2 = True, importGOBJ = True, importTextures = True, useTerrainBlending = True, createEmissiveMaterials = True):
         self.useAlpha = useAlpha
         self.createVertexGroups = createVertexGroups
         self.allowDuplicates = allowDuplicates
@@ -86,7 +86,7 @@ class ImportWoWOBJ(bpy.types.Operator, ImportHelper):
     createVertexGroups: bpy.props.BoolProperty(name = 'Create Vertex Groups', description = 'Create vertex groups for submeshes', default = 0)
     allowDuplicates: bpy.props.BoolProperty(name = 'Allow Duplicates (ADT)', description = 'Bypass the duplicate M2/WMO protection for ADT tiles', default = 0)
     useTerrainBlending: bpy.props.BoolProperty(name = 'Use terrain blending', description = 'Blend terrain textures using exported alpha maps', default = 1)
-    createEmissiveMaterials: bpy.props.BoolProperty(name = 'Create emissive materials', description = 'When applicable based on the material\'s blending mode. Might be less compatible when exporting to use in other software', default = 0)
+    createEmissiveMaterials: bpy.props.BoolProperty(name = 'Create emissive materials', description = 'When applicable based on the material\'s blending mode. Might be less compatible when exporting to use in other software', default = 1)
 
     def execute(self, context):
         settings = Settings(
