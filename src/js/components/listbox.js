ff9fb1f3fe9756132d402507c1d9df8a040a3ae4
NoodleBug <63314982+NoodleBug@users.noreply.github.com>
2025-02-01 11:25:16 -0600

    Formatting

--- src/js/components/listbox.js
+++ src/js/components/listbox.js
@@ -241,7 +241,9 @@ Vue.component('listbox', {
 			const child = this.$refs.root.querySelector('.item');
 
 			if (child !== null) {
-				const scrollCount = core.view.config.scrollSpeed === 0 ? Math.floor(this.$refs.root.clientHeight / child.clientHeight) : core.view.config.scrollSpeed;
+				const scrollCount = core.view.config.scrollSpeed === 0 ?  
+					Math.floor(this.$refs.root.clientHeight / child.clientHeight) : 
+					core.view.config.scrollSpeed;
 				const direction = e.deltaY > 0 ? 1 : -1;
 				this.scroll += ((scrollCount * this.itemWeight) * weight) * direction;
 				this.recalculateBounds();
