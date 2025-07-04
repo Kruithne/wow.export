03d6ec3b3adb991057f9a8e2feb343fdfdbd6a2a
Kruithne <kruithne@gmail.com>
2025-07-04 14:42:21 +0100

    add version to CLI and build with NativeAoT, SFE and no debug

--- cli/cli.cs
+++ cli/cli.cs
@@ -1,9 +1,12 @@
+using System.Reflection;
+
 namespace wow_export;
 
 class CLI
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, world!");
+		var version = Assembly.GetExecutingAssembly().GetName().Version;
+		Console.WriteLine($"wow_export_cli v{version}");
 	}
 }
