e180330c9625b6619cd737d2c48939eb13d754ed
Kruithne <kruithne@gmail.com>
2025-09-01 13:39:11 +0100

    create tarballs for linux/osx builds, resolves #442

--- publish.js
+++ publish.js
@@ -14,7 +14,6 @@ const PUBLISH_DIR = path.join(__dirname, 'publish');
 
 const argv = process.argv.splice(2);
 const PUBLISH_BUILDS = argv.length > 0 ? argv : ['win-x64', 'linux-x64', 'osx-x64'];
-const ZIP_FORMAT = 'wow-export-%s-%s.zip';
 
 for (const build_tag of PUBLISH_BUILDS) {
 	try {
@@ -30,9 +29,29 @@ for (const build_tag of PUBLISH_BUILDS) {
 		await fs.rename(update_file_path, path.join(pub_dir, 'update'));
 		await fs.rename(update_manifest_path, path.join(pub_dir, 'update.json'));
 		
-		const zip = new AdmZip();
-		zip.addLocalFolder(build_dir_path);
-		zip.writeZip(path.join(pub_dir, util.format(ZIP_FORMAT, build_tag, manifest.version)));
+		const archive_format = 'wow-export-%s-%s' + (build_tag === 'win-x64' ? '.zip' : '.tar.gz');
+		const archive_path = path.join(pub_dir, util.format(archive_format, build_tag, manifest.version));
+		
+		if (build_tag === 'win-x64') {
+			const zip = new AdmZip();
+			zip.addLocalFolder(build_dir_path);
+			zip.writeZip(archive_path);
+		} else {
+			const proc = Bun.spawn({
+				cmd: [
+					'tar',
+					'-czf', 
+					archive_path, 
+					'-C', 
+					path.dirname(build_dir_path), 
+					path.basename(build_dir_path)
+				]
+			});
+
+			await proc.exited;
+			if (proc.exitCode !== 0)
+				throw new Error(`tar process exited with code ${proc.exitCode}: ${proc.stderr?.toString() || 'Unknown error'}`);
+		}
 		
 		console.log(`successfully published build ${build_tag}`);
 	} catch (e) {
