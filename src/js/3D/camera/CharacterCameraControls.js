061c29f6ed730878c1c9cfa9d5ac07515b6c4168
Kruithne <kruithne@gmail.com>
2025-11-20 15:51:48 +0000

    implement zooming for character viewer

--- src/js/3D/camera/CharacterCameraControls.js
+++ src/js/3D/camera/CharacterCameraControls.js
@@ -5,6 +5,9 @@
  */
 
 const ROTATE_SPEED = 0.005;
+const ZOOM_SCALE = 0.95;
+const MIN_DISTANCE = 0.5;
+const MAX_DISTANCE = 100;
 
 class CharacterCameraControls {
 	constructor(camera, dom_element, render_group) {
@@ -19,9 +22,10 @@ class CharacterCameraControls {
 		this.mouse_down_handler = e => this.on_mouse_down(e);
 		this.mouse_move_handler = e => this.on_mouse_move(e);
 		this.mouse_up_handler = e => this.on_mouse_up(e);
+		this.mouse_wheel_handler = e => this.on_mouse_wheel(e);
 
 		this.dom_element.addEventListener('mousedown', this.mouse_down_handler);
-		this.dom_element.addEventListener('wheel', e => e.preventDefault());
+		this.dom_element.addEventListener('wheel', this.mouse_wheel_handler);
 	}
 
 	on_mouse_down(e) {
@@ -57,12 +61,34 @@ class CharacterCameraControls {
 		}
 	}
 
+	on_mouse_wheel(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		const direction = this.camera.position.clone().sub(this.target).normalize();
+		const current_distance = this.camera.position.distanceTo(this.target);
+
+		let zoom_amount;
+		if (e.deltaY < 0)
+			zoom_amount = current_distance * (1 - ZOOM_SCALE);
+		else if (e.deltaY > 0)
+			zoom_amount = -current_distance * (1 - ZOOM_SCALE);
+		else
+			return;
+
+		const new_distance = current_distance - zoom_amount;
+
+		if (new_distance >= MIN_DISTANCE && new_distance <= MAX_DISTANCE)
+			this.camera.position.addScaledVector(direction, -zoom_amount);
+	}
+
 	update() {
 		// no-op
 	}
 
 	dispose() {
 		this.dom_element.removeEventListener('mousedown', this.mouse_down_handler);
+		this.dom_element.removeEventListener('wheel', this.mouse_wheel_handler);
 		document.removeEventListener('mousemove', this.mouse_move_handler);
 		document.removeEventListener('mouseup', this.mouse_up_handler);
 	}
