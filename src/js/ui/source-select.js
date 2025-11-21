a417dff2f2cad199433b90473e14a496e601a4ed
Martin Benjamins <ictmartin@gmail.com>
2025-11-21 18:54:33 +0100

    FIx remote source select pings not working

--- src/js/ui/source-select.js
+++ src/js/ui/source-select.js
@@ -70,7 +70,7 @@ core.events.once('screen-source-select', async () => {
 
 	// Iterate CDN regions and create data nodes.
 	for (const region of constants.PATCH.REGIONS) {
-		let cdnURL = util.format(constants.PATCH.HOST, region);
+		let cdnURL = util.format(constants.PATCH.HOST, region.tag);
 		if(region === 'cn')
 			cdnURL = constants.PATCH.HOST_CHINA;
 
